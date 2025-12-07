860907151cde809cd72ef950d9a6a2c4a72eeade
Reshma-Shrestha777 <shrestha.reshma777@gmail.com>
2025-12-07 13:26:03 +0545

    Feat: User logout API implemented

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -43,8 +43,16 @@ export const login = async (req,res) => {
             sameSite: "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
-        return res.status(201).json(user)
+        return res.status(200).json(user)
     } catch (error) {
         return res.status(500).json({message:`login error ${error}`})
+    }
+}
+export const logOut = async (req,res) => {
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({message:"Logout Succesful!"})
+    } catch (error) {
+        return res.status(500).json({message:`logout error ${error}`})
     }
 }
